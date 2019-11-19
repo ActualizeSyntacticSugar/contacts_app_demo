0546589d989452654e3840e8528aaa8605acd4a4
Peter Jang <peter.x.jang@gmail.com>
2019-11-19 12:59:38 -0600

    Add contact model with seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+contact = Contact.new(first_name: "Peter", middle_name: "X", last_name: "Jang", email: "peter@email.com", phone_number: "232432432")
+contact.save
+contact = Contact.new(first_name: "Jay", middle_name: "Michael", last_name: "Wengrow", email: "jay@email.com", phone_number: "232432432")
+contact.save
+contact = Contact.new(first_name: "Grant", middle_name: "Charles", last_name: "Kazan", email: "grant@email.com", phone_number: "232432432")
+contact.save
+contact = Contact.new(first_name: "Jennifer", middle_name: "June", last_name: "Lubejko", email: "jennifer@email.com", phone_number: "232432434")
+contact.save
+contact = Contact.new(first_name: "Kevin", middle_name: "‹McCallister", last_name: "Mitchell", email: "kevin@email.com", phone_number: "232432439")
+contact.save
