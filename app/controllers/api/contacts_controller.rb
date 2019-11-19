06f302f2573bd718a877bb3827a840a593e14f7f
Peter Jang <peter.x.jang@gmail.com>
2019-11-19 14:08:44 -0600

    Add contacts RESTful API actions with model methods, validations, and active record queries

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,58 @@
+class Api::ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+
+    if params[:search]
+      @contacts = @contacts.where(
+        "first_name ILIKE ? OR last_name ILIKE ? OR email ILIKE ?",
+        "%#{params[:search]}%",
+        "%#{params[:search]}%",
+        "%#{params[:search]}%",
+      )
+    end
+
+    render "index.json.jb"
+  end
+
+  def create
+    @contact = Contact.new({
+      first_name: params[:first_name],
+      middle_name: params[:middle_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      bio: params[:bio],
+    })
+    if @contact.save
+      render "show.json.jb"
+    else
+      render json: { errors: @contact.errors.full_messages }, status: 422
+    end
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.bio = params[:bio] || @contact.bio
+    if @contact.save
+      render "show.json.jb"
+    else
+      render json: { errors: @contact.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+    render json: { message: "Contact successfully destroyed!" }
+  end
+end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,17 @@
 class Contact < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  def friendly_updated_at
+    updated_at.strftime("%B %e, %Y")
+  end
+
+  def full_name
+    "#{last_name}, #{first_name} #{middle_name}"
+  end
+
+  def japan_phone_number
+    "+81 #{phone_number}"
+  end
 end
